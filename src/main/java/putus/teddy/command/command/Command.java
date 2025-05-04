6af85354b57216fcf08d1b29a2b40fc7b64e9236
Teddy Putus <teddy.putus@sky.uk>
2025-05-04 22:23:39 +0100

    Add repositories to command interface

--- src/main/java/putus/teddy/command/command/Command.java
+++ src/main/java/putus/teddy/command/command/Command.java
@@ -1,5 +1,14 @@
 package putus.teddy.command.command;
 
+import putus.teddy.data.entity.*;
+import putus.teddy.data.repository.InMemoryRepository;
+
 public interface Command {
+    InMemoryRepository<CustomerPurchaseEntity> customerPurchaseRepository = new InMemoryRepository<>();
+    InMemoryRepository<InventoryEntity> inventoryRepository = new InMemoryRepository<>();
+    InMemoryRepository<FinancialEntity> financialRepository = new InMemoryRepository<>();
+    InMemoryRepository<SupplierEntity> supplierRepository = new InMemoryRepository<>();
+    InMemoryRepository<SupplierPurchaseEntity> supplierPurchaseRepository = new InMemoryRepository<>();
+
     boolean execute();
 }
