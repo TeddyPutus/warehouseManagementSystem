288335483a1bbdb3932395e1fb2f04f24766508b
Teddy Putus <teddy.putus@sky.uk>
2025-05-05 12:57:46 +0100

    Update Main

--- src/main/java/putus/teddy/Main.java
+++ src/main/java/putus/teddy/Main.java
@@ -1,11 +1,19 @@
 package putus.teddy;
 
+import putus.teddy.command.command.Command;
 import putus.teddy.command.parser.CommandParser;
-
+import putus.teddy.command.parser.CommandType;
+import putus.teddy.data.entity.*;
 import putus.teddy.command.registry.CommandRegistry;
+import putus.teddy.data.repository.InMemoryRepository;
+
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
+        setUpTestData();
+
         CommandRegistry commandRegistry = new CommandRegistry();
 
         boolean finished = false;
@@ -16,6 +24,25 @@ public class Main {
         while (!finished) {
             finished = commandRegistry.processCommand(CommandParser.parseCommand());
         }
+    }
+
+    public static void setUpTestData(){
+        Command.supplierRepository.importData(List.of(
+                new SupplierEntity("Supplier A", "123-456-7890", "supplier_a@email.com"),
+                new SupplierEntity("Supplier B", "987-654-3210", "supplier_b@email.com"),
+                new SupplierEntity("Supplier C", "555-555-5555", "supplier_c@email.com")));
+
+        Command.inventoryRepository.importData(List.of(
+                new InventoryEntity("item1",  10, 200.0),
+                new InventoryEntity("item2", 20, 400.0),
+                new InventoryEntity("item3",  30, 60.0),
+                new InventoryEntity("item4", 4, 600.0)));
+
+        Command.financialRepository.importData(List.of(
+                new FinancialEntity("item1", 10, 0, 100.0, 0.0),
+                new FinancialEntity("item2", 20, 0, 200.0, 0.0),
+                new FinancialEntity("item3", 30, 0, 300.0, 0.0),
+                new FinancialEntity("item4", 4, 0, 300.0, 0.0)));
 
     }
 }
