18952cffadfa11d357ca6c8b1b4404d6f5d987bb
Teddy Putus <teddy.putus@sky.uk>
2025-05-05 13:06:21 +0100

    Refactor Find and Register

---
+++ src/main/java/putus/teddy/command/command/FindStockOrders.java
@@ -0,0 +1,21 @@
+package putus.teddy.command.command;
+
+import putus.teddy.data.builder.QueryBuilder;
+import putus.teddy.data.entity.SupplierPurchaseEntity;
+import putus.teddy.data.repository.InMemoryRepository;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class FindStockOrders implements Command {
+     public boolean execute() {
+         System.out.println("Finding Stock Orders, please enter optional filter values...");
+
+         Stream<SupplierPurchaseEntity> supplierPurchaseStream = supplierPurchaseRepository.findMany(QueryBuilder.supplierPurchaseQuery());
+
+         SupplierPurchaseEntity.printTableHead();
+         supplierPurchaseStream.forEach(SupplierPurchaseEntity::printTableRow);
+
+         return false;
+     }
+}
