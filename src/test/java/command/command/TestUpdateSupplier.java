0ba2538d228330a4760a11aa3bf4ffc0784aca58
Teddy Putus <teddy.putus@sky.uk>
2025-05-05 12:57:39 +0100

    Update Supplier Command

---
+++ src/test/java/command/command/TestUpdateSupplier.java
@@ -0,0 +1,69 @@
+package command.command;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import putus.teddy.command.command.UpdateSupplier;
+import putus.teddy.data.builder.QueryBuilder;
+import putus.teddy.data.entity.SupplierEntity;
+import putus.teddy.data.parser.InputParser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class TestUpdateSupplier {
+    ByteArrayOutputStream outContent;
+    UpdateSupplier command = new UpdateSupplier();
+
+    static SupplierEntity entity1 = new SupplierEntity("item1", "1234", "email");
+
+    @BeforeClass
+    public static void classSetUp() {
+        UpdateSupplier.supplierRepository.create(entity1);
+    }
+
+    @Before
+    public void testSetUp() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    public void testUpdateSupplier() {
+
+        try (MockedStatic<InputParser> mockParser = org.mockito.Mockito.mockStatic(InputParser.class);
+             MockedStatic<QueryBuilder> mockedBuilder = org.mockito.Mockito.mockStatic(QueryBuilder.class)) {
+            mockParser.when(
+                    () -> InputParser.parseString("Supplier ID", true)
+            ).thenReturn(entity1.getId());
+
+            mockedBuilder.when(QueryBuilder::supplierQuery).thenReturn(Map.of(
+                    "phoneNumber", "5678"
+            ));
+
+            command.execute();
+            String output = outContent.toString();
+            assertFalse(output.contains("Supplier not found."));
+            assertTrue(output.contains("Supplier information updated successfully."));
+            assertEquals("5678", entity1.getPhoneNumber());
+        }
+    }
+
+    @Test
+    public void testUpdateSupplierNotFound() {
+        try (MockedStatic<InputParser> mockParser = org.mockito.Mockito.mockStatic(InputParser.class)) {
+            mockParser.when(
+                    () -> InputParser.parseString("Supplier ID", true)
+            ).thenReturn("non-existing-id");
+
+            command.execute();
+            String output = outContent.toString();
+            assertTrue(output.contains("Supplier not found."));
+            assertFalse(output.contains("Supplier information updated successfully."));
+        }
+    }
+}
